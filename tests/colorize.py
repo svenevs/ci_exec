ada488b2128c174b9ae74d45d656a8a43efda5a3
Stephen McDowell <svenevs.dev@gmail.com>
2019-04-06 22:08:30 -0700

    fix Styles.Regular not colorizing, misc other color stuff
    - Reduce insane number of tests for log_phase
    - Test the all_{colors,styles} methods
    - Define Colors before Styles xD
    - Sanity check and utility function: dump_predefined_color_styles

--- tests/colorize.py
+++ tests/colorize.py
@@ -15,13 +15,47 @@
 ########################################################################################
 """Tests for the :mod:`ci_exec.colorize` module."""
 
+import re
 import shutil
 import sys
 from typing import Any, Dict, Optional
 
-from ci_exec.colorize import Ansi, Colors, Styles, colorize, log_stage
+from ci_exec.colorize import Ansi, Colors, Styles, colorize, \
+    dump_predefined_color_styles, log_stage
 
 import pytest
+
+
+def test_all_colors():
+    """
+    Validate |all_colors| returns all available colors.
+
+    .. |all_colors| replace::
+
+        :func:`Colors.all_colors <ci_exec.colorize.Colors.all_colors>`
+    """
+    reported_all_colors = Colors.all_colors()
+    assert len(set(reported_all_colors)) == len(reported_all_colors)
+    all_colors = set([
+        item for key, item in Colors.__dict__.items() if key[0].isupper()
+    ])
+    assert set(reported_all_colors) == all_colors
+
+
+def test_all_styles():
+    """
+    Validate |all_styles| returns all available styles.
+
+    .. |all_styles| replace::
+
+        :func:`Styles.all_styles <ci_exec.colorize.Styles.all_styles>`
+    """
+    reported_all_styles = Styles.all_styles()
+    assert len(set(reported_all_styles)) == len(reported_all_styles)
+    all_styles = set([
+        item for key, item in Styles.__dict__.items() if key[0].isupper()
+    ])
+    assert set(reported_all_styles) == all_styles
 
 
 @pytest.mark.parametrize(
@@ -33,11 +67,58 @@ def test_colorize(color: str, style: str):
     message = "colors!"
     colored = colorize(message, color=color, style=style)
 
-    assert colored.startswith("{Escape}{color};{style}".format(
-        Escape=Ansi.Escape, color=color, style=style
-    ))
+    if style == Styles.Regular:
+        assert colored.startswith("{Escape}{color}m".format(
+            Escape=Ansi.Escape, color=color
+        ))
+    else:
+        assert colored.startswith("{Escape}{color};{style}m".format(
+            Escape=Ansi.Escape, color=color, style=style
+        ))
     assert colored.endswith("{Clear}".format(Clear=Ansi.Clear))
     assert message in colored
+
+
+def test_dump_predefined_color_styles(capsys):
+    """Validate :func:`~ci_exec.colorize.dump_predefined_color_styles` dumps all."""
+    dump_predefined_color_styles()
+    captured = capsys.readouterr()
+    assert captured.err == ""
+
+    # Collect the printed results.
+    colors_seen = {}
+    spec_regex = re.compile(r"color=([a-zA-Z]+), style=([a-zA-Z]+)")
+    for match in spec_regex.finditer(captured.out):
+        color, style = match.groups()
+        if color in colors_seen:
+            colors_seen[color].append(style)
+        else:
+            colors_seen[color] = [style]
+
+    # assert captured.out == "?"
+
+    # Make sure every color in every style was presented.
+    all_colors = Colors.all_colors()
+    all_styles = set(Styles.all_styles())
+
+    assert len(colors_seen) == len(all_colors)
+    for color_name, style_names in colors_seen.items():
+        # The style names are printed, get the values
+        styles_seen = [getattr(Styles, style) for style in style_names]
+        assert len(styles_seen) == len(style_names)
+        assert set(styles_seen) == all_styles
+
+        # Check the actual colors showed up.
+        color = getattr(Colors, color_name)
+        for style_name, style in zip(style_names, styles_seen):
+            expected = colorize(
+                "color={color_name}, style={style_name}".format(
+                    color_name=color_name, style_name=style_name
+                ),
+                color=color,
+                style=style
+            )
+            assert expected in captured.out
 
 
 @pytest.mark.parametrize(
@@ -45,14 +126,13 @@ def test_colorize(color: str, style: str):
     [
         # This is TOTAL overkill lol
         (stage, fill_char, color, style, width)
-        # In the ideal world right? Hehe.
         for stage in (
-            "CMake.Configure", "CMake.Build", "CMake.Test", "CMake.Install",
+            "CMake.Configure", "CMake.Build",
             # These are for testing the "phase message too long" scenarios.
             "M" * 42, "M" * 44, "M" * 256
         )
-        for fill_char in (None, "-", "_")
-        for color in (None, Colors.Green, Colors.Cyan, Colors.Magenta)
+        for fill_char in (None, "-")
+        for color in (None, Colors.Green, Colors.Cyan)
         for style in (Styles.Regular, Styles.Bold, Styles.Inverted)
         for width in (None, 44, 88)
     ]
