880fbd3cc1258034e1642a03e7607c79bad5d954
Stephen McDowell <svenevs.dev@gmail.com>
2021-08-20 17:22:56 -0600

    update license year

--- tests/colorize.py
+++ tests/colorize.py
@@ -1,5 +1,5 @@
 ########################################################################################
-# Copyright 2019 Stephen McDowell                                                      #
+# Copyright 2019-2021 Stephen McDowell                                                 #
 #                                                                                      #
 # Licensed under the Apache License, Version 2.0 (the "License");                      #
 # you may not use this file except in compliance with the License.                     #
