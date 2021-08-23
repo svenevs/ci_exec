a95ba457fd123941503675eaeb907b166c49b350
Stephen McDowell <svenevs.dev@gmail.com>
2021-08-22 19:51:25 -0600

    v0.1.3dev version bump

--- ci_exec/__init__.py
+++ ci_exec/__init__.py
@@ -29,7 +29,7 @@ from .patch import filter_file, unified_diff
 from .provider import Provider
 from .utils import cd, merge_kwargs, set_env, unset_env
 
-__version__ = "0.1.2"
+__version__ = "0.1.3.dev"
 __all__ = [
     # Core imports from ci_exec.colorize module.
     "Ansi", "Colors", "Styles", "colorize", "log_stage",
